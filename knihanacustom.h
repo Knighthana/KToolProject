ec61a3c0bd504dd47f5e9cc5f8fb758193259b2d
Knighthana <knighthana@outlook.com>
2021-05-08 19:32:04 +0800

    added my custom CPL header file.

---
+++ knihanacustom.h
@@ -0,0 +1,17 @@
+#ifndef _KNIHANACUSTOM_H_
+#define _KNIHANACUSTOM_H_
+
+#ifndef __type_status
+#define __type_status
+typedef int STATUS;
+#endif
+
+#ifndef __cplusplus
+#ifndef _BOOL
+typedef unsigned char bool;
+static const bool true = 1;
+static const bool false = 0;
+#endif
+#endif
+
+#endif
